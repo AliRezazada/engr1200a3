bc4cc55092828cec0d0cb64c7607542690a5a8bd
drqmahmoud <32275858+drqmahmoud@users.noreply.github.com>
2023-07-16 10:15:16 -0400

    Add files via upload

---
+++ MyApp.cpp
@@ -0,0 +1,32 @@
+// your copyright...description....includes...etc
+
+// Function prototypes
+void printMenu(); // prints menu when program starts
+
+// the login function authenticates the user, but
+// if the user provided incorrect username or password then
+// it allows the user to sign up. In other words, if the
+// user does not have an account it allows the user to signup.
+// User login data shoud be saved in a file.
+bool login(string username, string password);
+
+// Add at least four more distinct and useful functions
+// for the core of your application and its services.
+// Add the function prototypes here.
+
+
+int main() {
+    // implement your solution...use the functions from the crypto.h header file
+    return (0);
+}
+
+// Implement the functions here
+void printMenu() {
+
+}
+
+bool login(string username, string password) {
+
+}
+
+// Implement the functions you declared above, before main()
